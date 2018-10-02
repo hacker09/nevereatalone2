cc9460312af551e138b7cbe20c306b0462d493a1
at-lambui <lam.bui@asiantech.vn>
2018-10-02 15:32:48 +0700

    push master

---
+++ app/src/main/java/com/example/lambui/nevereatalone/ExtraIntent.java
@@ -0,0 +1,7 @@
+package com.example.lambui.nevereatalone;
+
+public final class ExtraIntent {
+    public static final String EXTRA_CHAT_REF = "EXTRA_CHAT_REF";
+    public static final String EXTRA_CURRENT_USER_ID = "EXTRA_CURRENT_USER_ID";
+    public static final String EXTRA_RECIPIENT_ID = "EXTRA_RECIPIENT_ID";
+}
